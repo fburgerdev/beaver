432493e1d5157da09177506ff21ea000c3c4e0da
Florian Burger <florianburger03@gmail.com>
2024-07-04 17:09:52 +0200

    fixed assert bug

--- include/beaver/beaver.hpp
+++ include/beaver/beaver.hpp
@@ -224,6 +224,6 @@ namespace Beaver {
 
 // #include "assert.hpp" (HPPMERGE)
 namespace Beaver {
-    #define ASSERT(condition) if (!condition) { LOG_FATAL(string("ASSERTION FAILED \"{}\""), (#condition)); exit(0); }
-    #define ASSERT_MSG(condition, format, ...) if (!condition) { LOG_FATAL(string("ASSERTION FAILED \"") + format + "\"", __VA_ARGS__); exit(0); }
+    #define ASSERT(condition) if (!(condition)) { LOG_FATAL(string("ASSERTION FAILED \"{}\""), (#condition)); exit(EXIT_FAILURE); }
+    #define ASSERT_MSG(condition, format, ...) if (!(condition)) { LOG_FATAL(string("ASSERTION FAILED \"") + format + "\"", __VA_ARGS__); exit(EXIT_FAILURE); }
 }
--- src/assert.hpp
+++ src/assert.hpp
@@ -2,6 +2,6 @@
 #include "logger.hpp"
 
 namespace Beaver {
-    #define ASSERT(condition) if (!condition) { LOG_FATAL(string("ASSERTION FAILED \"{}\""), (#condition)); exit(0); }
-    #define ASSERT_MSG(condition, format, ...) if (!condition) { LOG_FATAL(string("ASSERTION FAILED \"") + format + "\"", __VA_ARGS__); exit(0); }
+    #define ASSERT(condition) if (!(condition)) { LOG_FATAL(string("ASSERTION FAILED \"{}\""), (#condition)); exit(EXIT_FAILURE); }
+    #define ASSERT_MSG(condition, format, ...) if (!(condition)) { LOG_FATAL(string("ASSERTION FAILED \"") + format + "\"", __VA_ARGS__); exit(EXIT_FAILURE); }
 }
